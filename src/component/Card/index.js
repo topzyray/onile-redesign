18fa5c2866558933e5142f63191748fb09d5f5e6
topzyray <topzyray@yahoo.com>
2023-11-21 13:34:59 +0100

    Card Component Updated

--- src/component/Card/index.js
+++ src/component/Card/index.js
@@ -6,7 +6,7 @@ import { useRef } from 'react'
 import { useIsVisible } from '@/app/_lib/useIsVisible';
 
 
-const Card = () => {
+const Card = ({imageUrl, altText, title, location, size, price}) => {
 
     const ref = useRef()
     const isVisible = useIsVisible(ref)
@@ -15,15 +15,15 @@ const Card = () => {
     <Link href="/properties/1" ref={ref} className={`bg-[#FAFAFA] hover:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300   transition-opacity ease-in duration-700 ${isVisible ? "opacity-100" : "opacity-0"}`}>
         <div className="flex flex-col content-center gap-3 rounded-lg w-[16.875rem] h-[23.627rem] border border-solid border-[#CECECE]  ">
             <div>
-                <Image src="/assets/house11.jpg" alt='Card Image' width={270} height={237} className='rounded-t-lg object-cover' />
+                <Image src={imageUrl} alt={altText} width={270} height={237} className='rounded-t-lg object-cover' />
             </div>
 
             <div className='flex flex-col w-[16.3123rem] h-[6.25rem] px-5 pt-4 gap-2 text-[#344054]'>
-                <h4 className='text-[0.875rem] font-semibold'>Modern 3 Bedroom Duplex</h4>
-                <p className='text-[0.75rem] font-normal'>123 Main Street, Anytown, Ekiti</p>
-                <p className='opacity-50 font-normal'>2300 sq ft</p>
+                <h4 className='text-[0.875rem] font-semibold'>{title}</h4>
+                <p className='text-[0.75rem] font-normal'>{location}</p>
+                <p className='opacity-50 font-normal'>{size}</p>
                 <div className='flex flex-row justify-between'>
-                    <h4 className='text-[0.875rem] font-bold'>$4,489,000</h4>
+                    <h4 className='text-[0.875rem] font-bold'>{price}</h4>
                     <p className='flex gap-1'>
                         <span>
                             <Image src="/assets/share.svg" alt='Share' width={24} height={24} className='' />
--- src/component/Feature/index.js
+++ src/component/Feature/index.js
@@ -1,6 +1,10 @@
 "use client";
 
 import Card from '../Card/index.js'
+import house1 from '/public/assets/house7.jpg'
+import house2 from '/public/assets/house2.jpg'
+import house3 from '/public/assets/house11.jpg'
+import house4 from '/public/assets/house4.jpg'
 
 
 const Feature = () => {
@@ -12,10 +16,10 @@ const Feature = () => {
               <h1 className='text-[#344054] text-[1.5rem] md:text-[2rem] font-bold'>Featured Listing</h1>
           </section>
           <section className="grid sm:grid-cols-2 xl:grid-cols-4 gap-5">
-              <Card className="ease-in duration-500" />
-              <Card />
-              <Card />
-              <Card />
+              <Card imageUrl={house1} altText="Image of Property 1" title="Modern 3 Bedroom Duplex" location="123, Olayinka Street, Ado Ekiti." size="2300 sq.ft" price="N 489,000" />
+              <Card imageUrl={house2} altText="Image of Property 2" title="Modern 4 Bedroom Duplex" location="47, Bamisile Avenue, Ikere Ekiti." size="5045 sq.ft" price="N 420,900" />
+              <Card imageUrl={house3} altText="Image of Property 3" title="Modern 3 Bedroom Flat" location="222, Akinloye Close, Are Ekiti." size="1500 sq.ft" price="N 350,500" />
+              <Card imageUrl={house4} altText="Image of Property 4" title="Modern 2 Bedroom Bungalow" location="22, Paul and Joshua Lane, Ado Ekiti" size="4300 sq.ft" price="N 600,000" />
           </section>
       </section>
     </section>
