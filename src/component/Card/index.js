5cb4b3c87f10428859474a1cef74743808772abf
topzyray <topzyray@yahoo.com>
2024-02-05 23:30:34 +0100

    Update effects on Card component and card details spacing

--- src/component/Card/index.js
+++ src/component/Card/index.js
@@ -14,13 +14,13 @@ const Card = ({imageUrl, altText, title, location, size, price}) => {
     const isVisible = useIsVisible(ref)
 
   return <>
-    <Link href="/properties/1" ref={ref} className={`bg-[#FAFAFA] focus:outline-none focus:ring focus:ring-gray-300   transform ease-in-out duration-500 scale-95 hover:scale-100 ${isVisible ? "hover:shadow-[0_2px_6px] hover:shadow-[#ff7f50] hover:border-none hover:rounded-md" : "shadow-none"}`}>
+    <Link href="/properties/1" ref={ref} className={`bg-[#FAFAFA] focus:outline-none focus:ring focus:ring-gray-300 focus:rounded-2xl   transform ease-in-out duration-500 scale-95 hover:scale-100 hover:opacity-90 ${isVisible ? "hover:shadow-[0_2px_6px] hover:shadow-[#ff7f50] hover:border-none hover:rounded-2xl" : "shadow-none"}`}>
         <div className="max-w-sm max-h-[24rem] w-full lg:w-[18.875rem] h-[23.627rem] flex flex-col gap-0 rounded-2xl border border-solid border-[#CECECE]  ">
             <div className='w-full h-[65%]'>
                 <Image src={imageUrl} alt={altText} className='w-full h-full rounded-t-2xl object-cover' />
             </div>
 
-            <div className='flex flex-col w-full h-[35%] px-5 pt-4 gap-2 text-[#344054]'>
+            <div className='flex flex-col w-full h-[35%] -space-y-1 px-5 py-4 gap-2 text-[#344054]'>
                 <h4 className='text-[0.875rem] font-semibold'>{title}</h4>
                 <p className='text-[0.75rem] font-normal'>{location}</p>
                 <p className='opacity-50 font-normal'>{size}</p>
